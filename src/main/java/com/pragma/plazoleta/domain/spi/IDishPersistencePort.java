94e4fb1e38af03d9db2be0e07799aabbadcc5a2f
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 01:26:20 -0500

    feat: add findById method to dish persistence port

--- src/main/java/com/pragma/plazoleta/domain/spi/IDishPersistencePort.java
+++ src/main/java/com/pragma/plazoleta/domain/spi/IDishPersistencePort.java
@@ -2,7 +2,11 @@ package com.pragma.plazoleta.domain.spi;
 
 import com.pragma.plazoleta.domain.model.Dish;
 
+import java.util.Optional;
+
 public interface IDishPersistencePort {
 
     Dish saveDish(Dish dish);
+
+    Optional<Dish> findById(Long id);
 }
