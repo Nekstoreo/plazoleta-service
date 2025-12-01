a93cc1c7d7b2dc1d60f64cfc5aace9f519cb0d82
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 01:11:46 -0500

    feat: add DishDtoMapper

---
+++ src/main/java/com/pragma/plazoleta/application/mapper/DishDtoMapper.java
@@ -0,0 +1,17 @@
+package com.pragma.plazoleta.application.mapper;
+
+import com.pragma.plazoleta.application.dto.request.DishRequestDto;
+import com.pragma.plazoleta.application.dto.response.DishResponseDto;
+import com.pragma.plazoleta.domain.model.Dish;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface DishDtoMapper {
+
+    Dish toDish(DishRequestDto dishRequestDto);
+
+    DishResponseDto toDishResponseDto(Dish dish);
+}
