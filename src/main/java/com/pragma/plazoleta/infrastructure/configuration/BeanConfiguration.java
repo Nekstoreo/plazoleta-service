c951849f62558ee8f1b700902e9c341035f2ef70
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 01:12:16 -0500

    config: register DishUseCase bean

--- src/main/java/com/pragma/plazoleta/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/plazoleta/infrastructure/configuration/BeanConfiguration.java
@@ -1,8 +1,11 @@
 package com.pragma.plazoleta.infrastructure.configuration;
 
+import com.pragma.plazoleta.domain.api.IDishServicePort;
 import com.pragma.plazoleta.domain.api.IRestaurantServicePort;
+import com.pragma.plazoleta.domain.spi.IDishPersistencePort;
 import com.pragma.plazoleta.domain.spi.IRestaurantPersistencePort;
 import com.pragma.plazoleta.domain.spi.IUserValidationPort;
+import com.pragma.plazoleta.domain.usecase.DishUseCase;
 import com.pragma.plazoleta.domain.usecase.RestaurantUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +18,12 @@ public class BeanConfiguration {
             IRestaurantPersistencePort restaurantPersistencePort,
             IUserValidationPort userValidationPort) {
         return new RestaurantUseCase(restaurantPersistencePort, userValidationPort);
+    }
+
+    @Bean
+    public IDishServicePort dishServicePort(
+            IDishPersistencePort dishPersistencePort,
+            IRestaurantPersistencePort restaurantPersistencePort) {
+        return new DishUseCase(dishPersistencePort, restaurantPersistencePort);
     }
 }
