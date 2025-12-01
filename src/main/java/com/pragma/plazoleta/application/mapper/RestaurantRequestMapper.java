48653cdf75cd4d28f6e361be29497a94ad2fdc70
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 00:54:52 -0500

    feat: add restaurant mappers

---
+++ src/main/java/com/pragma/plazoleta/application/mapper/RestaurantRequestMapper.java
@@ -0,0 +1,13 @@
+package com.pragma.plazoleta.application.mapper;
+
+import com.pragma.plazoleta.application.dto.request.CreateRestaurantRequest;
+import com.pragma.plazoleta.domain.model.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface RestaurantRequestMapper {
+
+    Restaurant toRestaurant(CreateRestaurantRequest request);
+}
---
+++ src/main/java/com/pragma/plazoleta/application/mapper/RestaurantResponseMapper.java
@@ -0,0 +1,13 @@
+package com.pragma.plazoleta.application.mapper;
+
+import com.pragma.plazoleta.application.dto.response.RestaurantResponse;
+import com.pragma.plazoleta.domain.model.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface RestaurantResponseMapper {
+
+    RestaurantResponse toResponse(Restaurant restaurant);
+}
