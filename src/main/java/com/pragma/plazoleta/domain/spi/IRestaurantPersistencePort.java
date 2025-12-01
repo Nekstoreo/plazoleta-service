b8d6f3d1de28930f37b6fc4b3c0f6cf91a29e138
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 01:11:31 -0500

    feat: add findById method to restaurant persistence port

--- src/main/java/com/pragma/plazoleta/domain/spi/IRestaurantPersistencePort.java
+++ src/main/java/com/pragma/plazoleta/domain/spi/IRestaurantPersistencePort.java
@@ -2,9 +2,13 @@ package com.pragma.plazoleta.domain.spi;
 
 import com.pragma.plazoleta.domain.model.Restaurant;
 
+import java.util.Optional;
+
 public interface IRestaurantPersistencePort {
 
     Restaurant saveRestaurant(Restaurant restaurant);
 
     boolean existsByNit(String nit);
+
+    Optional<Restaurant> findById(Long id);
 }
