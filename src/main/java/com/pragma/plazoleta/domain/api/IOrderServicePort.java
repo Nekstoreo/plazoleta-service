3ff5aff5a2f1568c51730f12b02f10158512c815
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 22:22:36 -0500

    feat: add pagination methods to order ports

--- src/main/java/com/pragma/plazoleta/domain/api/IOrderServicePort.java
+++ src/main/java/com/pragma/plazoleta/domain/api/IOrderServicePort.java
@@ -1,8 +1,12 @@
 package com.pragma.plazoleta.domain.api;
 
 import com.pragma.plazoleta.domain.model.Order;
+import com.pragma.plazoleta.domain.model.OrderStatus;
+import com.pragma.plazoleta.domain.model.PagedResult;
 
 public interface IOrderServicePort {
 
     Order createOrder(Order order);
+
+    PagedResult<Order> getOrdersByRestaurantAndStatus(Long employeeId, OrderStatus status, int page, int size);
 }
--- src/main/java/com/pragma/plazoleta/domain/spi/IOrderPersistencePort.java
+++ src/main/java/com/pragma/plazoleta/domain/spi/IOrderPersistencePort.java
@@ -1,10 +1,14 @@
 package com.pragma.plazoleta.domain.spi;
 
 import com.pragma.plazoleta.domain.model.Order;
+import com.pragma.plazoleta.domain.model.OrderStatus;
+import com.pragma.plazoleta.domain.model.PagedResult;
 
 public interface IOrderPersistencePort {
 
     Order saveOrder(Order order);
 
     boolean existsActiveOrderByClientId(Long clientId);
+
+    PagedResult<Order> findByRestaurantIdAndStatusPaginated(Long restaurantId, OrderStatus status, int page, int size);
 }
