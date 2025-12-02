22ca53bbc5f16d41c2f841dba2ea177e85a1944d
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 21:48:36 -0500

    feat: add order service and persistence ports

---
+++ src/main/java/com/pragma/plazoleta/domain/api/IOrderServicePort.java
@@ -0,0 +1,8 @@
+package com.pragma.plazoleta.domain.api;
+
+import com.pragma.plazoleta.domain.model.Order;
+
+public interface IOrderServicePort {
+
+    Order createOrder(Order order);
+}
---
+++ src/main/java/com/pragma/plazoleta/domain/spi/IOrderPersistencePort.java
@@ -0,0 +1,10 @@
+package com.pragma.plazoleta.domain.spi;
+
+import com.pragma.plazoleta.domain.model.Order;
+
+public interface IOrderPersistencePort {
+
+    Order saveOrder(Order order);
+
+    boolean existsActiveOrderByClientId(Long clientId);
+}
