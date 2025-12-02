6eb902cf48d3de7721a1522b34428272631a8b0e
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-01 22:22:51 -0500

    feat: add feign adapter for employee restaurant

---
+++ src/main/java/com/pragma/plazoleta/infrastructure/output/feign/adapter/EmployeeRestaurantFeignAdapter.java
@@ -0,0 +1,29 @@
+package com.pragma.plazoleta.infrastructure.output.feign.adapter;
+
+import com.pragma.plazoleta.domain.spi.IEmployeeRestaurantPort;
+import com.pragma.plazoleta.infrastructure.output.feign.client.IUserFeignClient;
+import com.pragma.plazoleta.infrastructure.output.feign.dto.UserDto;
+import feign.FeignException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class EmployeeRestaurantFeignAdapter implements IEmployeeRestaurantPort {
+
+    private final IUserFeignClient userFeignClient;
+
+    public EmployeeRestaurantFeignAdapter(IUserFeignClient userFeignClient) {
+        this.userFeignClient = userFeignClient;
+    }
+
+    @Override
+    public Optional<Long> getRestaurantIdByEmployeeId(Long employeeId) {
+        try {
+            return userFeignClient.getUserById(employeeId)
+                    .map(UserDto::getRestaurantId);
+        } catch (FeignException e) {
+            return Optional.empty();
+        }
+    }
+}
