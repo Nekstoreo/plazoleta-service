00dfdc26d9a59802e7f6117c51bd1114bbeba8c8
Nestor Gutierrez <nestorg456k@outlook.com>
2025-12-03 11:22:44 -0500

    refactor: remove unused parameterized constructor from Traceability models

--- src/main/java/com/pragma/plazoleta/domain/model/Traceability.java
+++ src/main/java/com/pragma/plazoleta/domain/model/Traceability.java
@@ -1,7 +1,12 @@
 package com.pragma.plazoleta.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 public class Traceability {
     private String id;
     private Long orderId;
@@ -15,88 +20,41 @@ public class Traceability {
 
     public Traceability() {
     }
+    
+    public static class ClientInfo {
+        private final Long id;
+        private final String email;
 
-    public Traceability(String id, Long orderId, Long clientId, String clientEmail, LocalDateTime date, String previousStatus, String newStatus, Long employeeId, String employeeEmail) {
-        this.id = id;
-        this.orderId = orderId;
-        this.clientId = clientId;
-        this.clientEmail = clientEmail;
-        this.date = date;
-        this.previousStatus = previousStatus;
-        this.newStatus = newStatus;
-        this.employeeId = employeeId;
-        this.employeeEmail = employeeEmail;
+        public ClientInfo(Long id, String email) {
+            this.id = id;
+            this.email = email;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+    
+    public static class EmployeeInfo {
+        private final Long id;
+        private final String email;
+
+        public EmployeeInfo(Long id, String email) {
+            this.id = id;
+            this.email = email;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Long getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(Long clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getClientEmail() {
-        return clientEmail;
-    }
-
-    public void setClientEmail(String clientEmail) {
-        this.clientEmail = clientEmail;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getPreviousStatus() {
-        return previousStatus;
-    }
-
-    public void setPreviousStatus(String previousStatus) {
-        this.previousStatus = previousStatus;
-    }
-
-    public String getNewStatus() {
-        return newStatus;
-    }
-
-    public void setNewStatus(String newStatus) {
-        this.newStatus = newStatus;
-    }
-
-    public Long getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Long employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getEmployeeEmail() {
-        return employeeEmail;
-    }
-
-    public void setEmployeeEmail(String employeeEmail) {
-        this.employeeEmail = employeeEmail;
-    }
 }
